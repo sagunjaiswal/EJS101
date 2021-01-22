54e9fdf980bfdca3f68216c26bd1c4e57e344486
SAGUN JAISWAL <63302439+sagunjaiswal@users.noreply.github.com>
2021-01-22 18:09:31 +0530

    final commit

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require("express");
 
 const app = express();
 
-let bullets = ["bullet1", "bullet2", "bullet3"];
+
 let items = [
     {col1:'1',col2:'<%= %>'},
     {col1:'2',col2:'<%- %>'},
@@ -15,7 +15,7 @@ let items = [
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.get('/', function(req, res){    
-    res.render('home',{ tabledata : items , title : "Rendering value into template", name: "Sagun", bullets: bullets});
+    res.render('index',{ tabledata : items , title : "Rendering value into template", name: "Sagun"});
 });
 
 app.listen(3000);
